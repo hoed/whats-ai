68b9467a33268fb216ac070302513e3ab72665ac
hoedhud <hoedhud@gmail.com>
2025-05-02 21:22:20 +0700

    update navbar sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -51,7 +51,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
   
   return (
     <aside className={`
-      fixed lg:static inset-y-0 left-0 z-40 
+      fixed lg:static inset-y-0 left-0 z-10 
       ${bgColor} ${borderColor} border-r
       transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} 
       lg:translate-x-0 transition-transform duration-300 ease-in-out
